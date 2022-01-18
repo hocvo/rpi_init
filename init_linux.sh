89c9ab7a7d2237f3f06971af28d3243bf2f5df51
Hoc Vo <chohocvo@gmail.com>
2022-01-18 06:43:46 +0000

    update init script

--- init_linux.sh
+++ init_linux.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # this script requires sudo
-echo "create ssh key on github to continue"
-#exit -1
-#sudo su
 sudo apt update
+
+if [[ $? -ne 0 ]]; then
+  exit 100
+fi
 sudo apt upgrade -y
 sudo apt install -y git
 
@@ -11,7 +12,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.unstage reset HEAD --
+git config --global alias.unstage "reset HEAD --"
 git config --global alias.logs "log --date=format:'%b-%d-%y' --pretty='%C(auto)%h%Creset %ad %C(black bold)%aN%Creset %s'"
 git config --global alias.logss "log --all --graph --date=format:'%b-%d-%y' --pretty='%C(auto)%h%Creset %ad %C(black bold)%aN%Creset %C(auto)%d %s'"
 git config --global alias.cp cherry-pick
@@ -19,8 +20,11 @@ git config --global user.email "chohocvo@gmail.com"
 git config --global user.name "Hoc Vo"
 
 sudo apt install -y vim
-cd ~
 #git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh vimrc/install_awesome_vimrc.sh
 mv ~/.bashrc ~/.bashrc.old
 cp bashrc ~/.bashrc
+source ~/.bashrc
+
+mkdir ~/.vim_runtime
+cp -r vim/* ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
