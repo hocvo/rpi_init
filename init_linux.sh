e9160f90a0366e71803485ffa1c38e8ae3096082
Hoc Vo <hocnvo@gmail.com>
2022-01-17 22:06:59 -0800

    restructured

--- init_linux.sh
+++ init_linux.sh
@@ -2,13 +2,25 @@
 # this script requires sudo
 echo "create ssh key on github to continue"
 #exit -1
-sudo su
-apt update
-apt upgrade -y
-apt install -y git
-apt install -y vim
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-git clone git@github.com:hocvo/rpi_init.git
-cp -r ./rpi_init ~/.
+#sudo su
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git
 
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage reset HEAD --
+git config --global alias.logs "log --date=format:'%b-%d-%y' --pretty='%C(auto)%h%Creset %ad %C(black bold)%aN%Creset %s'"
+git config --global alias.logss "log --all --graph --date=format:'%b-%d-%y' --pretty='%C(auto)%h%Creset %ad %C(black bold)%aN%Creset %C(auto)%d %s'"
+git config --global alias.cp cherry-pick
+git config --global user.email "chohocvo@gmail.com"
+git config --global user.name "Hoc Vo"
+
+sudo apt install -y vim
+cd ~
+#git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh vimrc/install_awesome_vimrc.sh
+mv ~/.bashrc ~/.bashrc.old
+cp bashrc ~/.bashrc
